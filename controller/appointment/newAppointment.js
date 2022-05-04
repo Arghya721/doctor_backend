f3b0a6b14b804d053569212c5366c37a3ca99bb5
Harsh Kumar <61644460+harshkumar950461@users.noreply.github.com>
2022-05-04 21:21:17 +0530

    Update newAppointment.js

--- controller/appointment/newAppointment.js
+++ controller/appointment/newAppointment.js
@@ -11,11 +11,11 @@ var createZoomMeeting = async() =>{
     iss: "7JqB9DRXSMSk_UeS-luIUg",
     exp: new Date().getTime() + 5000,
   };
-  const token = jwt.sign(payload, "ENTER ZOOM KEY");
+  const token = jwt.sign(payload, "PEF8elFhiMXF1We8lqzcXecoaWxV5JrD4HXy");
   let meetingDetails = {};
   var options = {
     method: "POST",
-    uri: "https://api.zoom.us/v2/users/" + "vikash14609@gmail.com" + "/meetings",
+    uri: "https://api.zoom.us/v2/users/" + "divyanshbansal13@gmail.com" + "/meetings",
     body: {
       topic: "Meeting",
       type: 1,
@@ -59,13 +59,7 @@ addAppointment = async(req, res) => {
     // taking input fomr req.body which send by client
     const{doctorID,patientID,concern,status,fee, bookingDate, appointmentDate,appointmentTime}=req.body ;
         try{
-        var meetingDetails = await createZoomMeeting();
-        if(meetingDetails=={} ||meetingDetails.meetingID === null || meetingDetails.meetingpwd ==null)
-          res.status(422).json({message:"Error occured while booking appointmnet"});
-        else{
-          console.log(meetingDetails);
-          let meetingID = meetingDetails.meetingID;
-          let meetingpwd = meetingDetails.meetingpwd;
+      
         var newAppointment = new Appointment({ doctorID,patientID,concern,status,fee, bookingDate, appointmentDate,appointmentTime, meetingID,meetingpwd});
         await newAppointment.save();
          // sending json to client side
