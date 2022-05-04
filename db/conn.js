2eaaf882915e29327a11b85a2e9d68137d3f374a
Vikash Kumar <86666736+vikashkumar2701@users.noreply.github.com>
2022-05-04 21:17:13 +0530

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -4,9 +4,9 @@ const DB = process.env.DATABASE;
 
 mongoose.connect(DB, {
     useNewUrlParser: true,
-   // useCreateIndex: true,
+
     useUnifiedTopology: true,
-  //  useFindAndModify: false
+
 }).then(() => {
     console.log(`connection successfull`);
 }).catch((err) => console.log(`No connection` + err));
